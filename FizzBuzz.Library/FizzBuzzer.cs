9e9287a0cb2b9b4b7e410e918fe28ca970c58987
David Barth <davidbarth80@gmail.com>
2017-03-21 11:18:00 +0000

    Passing Fizz

--- FizzBuzz.Library/FizzBuzzer.cs
+++ FizzBuzz.Library/FizzBuzzer.cs
@@ -1,15 +1,16 @@
-﻿using System;
-
-namespace FizzBuzz.Library
+﻿namespace FizzBuzz.Library
 {
     //Class representing the FizzBuzzer object that is under test
     public class FizzBuzzer
     {
         public static string GetValue(int input)
         {
-            return input.ToString();
-        }
+            if (input == 3)
+            {
+                return "Fizz";
+            }
 
-       
-    }
+            return input.ToString();
+         }
+     }
 }
